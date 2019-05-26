8dfd10a1a8a43c98fb7680cd31a522e74090f64f
Danish Jafri <danishjafri88@gmail.com>
2019-05-26 21:08:20 +0530

    Initial Commit

---
+++ QR Nominal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR Nominal
+//
+//  Created by Danish Jafri on 26/05/19.
+//  Copyright © 2019 brave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
