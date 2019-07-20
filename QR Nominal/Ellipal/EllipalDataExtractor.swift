f28809e4241db8957db78cc3fbaad3d64fd1f042
Marshall T. Rose <mrose17@gmail.com>
2019-07-20 09:05:24 -0500

    Update EllipalDataExtractor.swift

--- QR Nominal/Ellipal/EllipalDataExtractor.swift
+++ QR Nominal/Ellipal/EllipalDataExtractor.swift
@@ -15,7 +15,6 @@ struct EllipalDataExtractor {
 
             return string!
         } catch {
-
             throw "Unable to encode data for display" + error.localizedDescription
         }
     }
